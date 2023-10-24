7c86021bcd6fec83026c1dfc707f8f49ef9b45bd
unknown <thanhquang633@gmail.com>
2023-10-24 14:58:04 +0700

    24/10/2023

---
+++ app/dao.py
@@ -0,0 +1,30 @@
+def load_categories():
+    return [{
+        'id': 1,
+        'name': 'iphone'
+    }, {
+        'id': 1,
+        'name': 'ipod'
+    }, {
+        'id': 1,
+        'name': 'ipad'
+    },]
+
+def load_product(kw=None):
+    return [{
+        'id': 1,
+        'name': 'iphone 13 pro max',
+        'price': 27000000,
+        'image': 'https://phongapple.vn/wp-content/uploads/2021/10/13v.png'
+    }, {
+        'id': 1,
+        'name': 'iphone 13 pro max',
+        'price': 27000000,
+        'image': 'https://phongapple.vn/wp-content/uploads/2021/10/13v.png'
+    }, {
+        'id': 1,
+        'name': 'iphone 13 pro max',
+        'price': 27000000,
+        'image': 'https://phongapple.vn/wp-content/uploads/2021/10/13v.png'
+    },]
+
